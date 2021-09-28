bc127c21ae911dc523f7bd1b90bb3be69b6beef7
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-09-28 12:13:12 +0200

    added deconvoluted data

---
+++ code/deconvolute_rolling_sum.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from tqdm.auto import tqdm
+tqdm.pandas()
+
+def deconvolute(y):
+    y = y.to_numpy()
+    start = np.repeat(y[0]//7, 6)
+    if y[0]%7 != 0:
+        start += np.append(np.repeat(0, 7 - y[0]%7), np.repeat(1, y[0]%7 -1))
+    y = np.append(start, y)
+    n = len(y)
+    
+    A = np.eye(n, n)
+    for k in range(1, 7):
+        A += np.eye(n, n, k)
+    A = A[:-6]
+    
+    initial_conditions = np.append(np.eye(6), np.zeros((6, n-6)), axis = 1)
+    A = np.append(initial_conditions, A, axis = 0)
+    
+    A_inv = np.linalg.inv(A) 
+    x = np.dot(A_inv,y)
+    x = x[6:]
+    
+    return x
+
+
+path = Path('../data-truth/COVID-19/rolling-sum/')
+files = [f.name for f in path.glob('**/*')]
+
+for f in tqdm(files, total = len(files)):
+    df = pd.read_csv(path/f)
+    df.dropna(inplace = True)
+    df.sort_values(['location', 'age_group', 'date'], inplace = True)
+    df.value = df.groupby(['location', 'age_group'])['value'].transform(deconvolute)
+    df.to_csv('../data-truth/COVID-19/deconvoluted/' + f.replace('.csv', '_deconvoluted.csv'), index = False)
+    
