3e44037df6aa3620ab565d55f4f067c8e4796a86
jbracher <jbracher@outlook.de>
2021-10-11 12:42:06 +0200

    Update app.

--- nowcast_viz_de/ui.R
+++ nowcast_viz_de/ui.R
@@ -5,10 +5,10 @@ mondays <- read.csv("plot_data/mondays.csv")$date
 
 # Define UI for application
 shinyUI(fluidPage(
-
+    
     # Application title
-    titlePanel("Hospitalization nowcasts"),
-
+    titlePanel("Nowcasting hospitalizations from COVID-19 in Germany"),
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
@@ -28,12 +28,36 @@ shinyUI(fluidPage(
                                        "Age 15 - 34" = "15-34",
                                        "Age 35 - 59" = "35-59",
                                        "Age 60 - 79" = "60-79",
-                                       "Age 80 and above" = "80+"), width = "200px")
+                                       "Age 80 and above" = "80+"), width = "200px"),
+            radioButtons("select_interval", label = "Show prediction interval:", 
+                         choices = c("95%", "50%", "none"), selected = "95%", inline = TRUE),
+            radioButtons("select_scale", label = "Show as:", 
+                         choices = c("absolute counts", "per 100.000"), selected = "absolute counts", inline = TRUE),
+            radioButtons("select_log", label = NULL, 
+                         choices = c("natural scale", "log scale"), selected = "natural scale", inline = TRUE)
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotlyOutput("tsplot")
+            p("Seven-day hospitalization incidences (as ",  
+            a("reported by RKI", href = "https://github.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland"),
+            "have become one of the main indicators for the
+              spread of COVID-19 in Germany. However, due to ", 
+              a("reporting lags", href = "https://www.rki.de/SharedDocs/FAQ/NCOV2019/FAQ_Liste_Epidemiologie.html"), 
+              ", the most recent data points
+              are incomplete and tend to underestimate the true number of hospitalization. This dashboard
+              summarizes outputs from different nowcasting models, i.e. statistical methods to correct
+              for reporting delays. Below, outputs from individual models and a combined ensemble nowcast
+              (nowcasthub-ensemble) are shown."),
+            p("This project is currently still in development and the
+              reliability of results has not yet been assessed systematically."),
+            plotlyOutput("tsplot"),
+            p(),
+            p("Raw forecast data and more information can be found in our",
+              a("GitHub repository.", href = "https://github.com/KITmetricslab/hospitalization-nowcast-hub")),
+            p("This platform is run by members of the ",
+              a("Chair of Statistics and Econometrics", href = "https://statistik.econ.kit.edu/index.php"),
+              "at Karlsruhe Institute of Technology. Contact: forecasthub@econ.kit.edu")
         )
     )
 ))
