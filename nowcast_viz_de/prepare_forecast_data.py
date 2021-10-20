f72d70e6aeeea26b4361559a0302fe52200f0c0d
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-10-20 15:19:16 +0200

    use latest submission (within 7 days) if not available for current date

--- nowcast_viz_de/prepare_forecast_data.py
+++ nowcast_viz_de/prepare_forecast_data.py
@@ -10,7 +10,6 @@ def process_forecasts(df):
                            'pathogen', 'model'], values='value', columns='quantile')
 
     df.columns.name = None
-
     df.reset_index(inplace=True)
 
     df['target_type'] = 'hosp'
@@ -25,22 +24,34 @@ def process_forecasts(df):
     return(df)
 
 path = Path('../data-processed')
+df_files = pd.DataFrame({'file': [f.name for f in path.glob('**/*') if f.name.endswith('.csv')]})
+df_files['date'] = pd.to_datetime(df_files.file.str[:10])
+df_files['model'] = df_files.file.str[11:-4]
 
-forecast_dates = pd.unique([f.name[:10] for f in path.glob('**/*') if f.name.endswith('.csv')])
+all_models = df_files.model.unique()
 
-models = [f.name for f in path.iterdir() if not f.name.endswith('.csv')]
-
-for date in forecast_dates:
+for date in sorted(df_files.date.unique()):
+    df_temp = df_files[df_files.date == date]
+    missing = [m for m in all_models if m not in df_temp.model.unique()]
+    
+    for m in missing:
+        df_old = df_files[(df_files.model == m) & (df_files.date.between(d - pd.Timedelta(days = 7), d))]
+        df_old = df_old[df_old.date == df_old.date.max()]
+        df_temp = df_temp.append(df_old)
+    
     dfs = []
-    for m in models:
-        p = path/m
-        forecasts = [file.name for file in p.iterdir() if date in file.name]
-        for f in forecasts:
-            df_temp = pd.read_csv(path/m/f)
-            df_temp['model'] = m
-            dfs.append(df_temp)
+    for _, row in df_temp.iterrows():
+        df_temp2 = pd.read_csv(f'../data-processed/{row.model}/{row.file}', parse_dates = ['target_end_date'])
+        df_temp2['model'] = row.model
+        dfs.append(df_temp2)
 
     df = pd.concat(dfs)
+    
+    df = df[df.target_end_date >= date - pd.Timedelta(days = 28)]
+
     df = process_forecasts(df)
     df.to_csv(f'plot_data/{date}_forecast_data.csv', index=False)
-    
+
+# save list of available teams
+df_models = pd.DataFrame({'model': all_models})
+df_models.to_csv('plot_data/list_teams.csv', index = False)
