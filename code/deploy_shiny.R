a944e58528a79f8f265780d070f0ec413de3d006
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2021-11-12 15:14:33 +0100

    Update deploy_shiny.R

--- code/deploy_shiny.R
+++ code/deploy_shiny.R
@@ -8,6 +8,5 @@ SHINYAPPS_NAME = Sys.getenv("SHINYAPPS_NAME")
 SHINYAPPS_TOKEN = Sys.getenv("SHINYAPPS_TOKEN")
 SHINYAPPS_SECRET = Sys.getenv("SHINYAPPS_SECRET")
 
-
 setAccountInfo(name=SHINYAPPS_NAME, token=SHINYAPPS_TOKEN, secret=SHINYAPPS_SECRET)
-deployApp(appDir = "../nowcast_viz_de", appName = "github-viz-test")
+deployApp(appDir = "./nowcast_viz_de", appName = "github-viz-test")
