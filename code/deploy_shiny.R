21fa7d17e7d417e539da5f2ae8d824179c399402
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2021-11-12 15:22:38 +0100

    Update deploy_shiny.R

--- code/deploy_shiny.R
+++ code/deploy_shiny.R
@@ -1,8 +1,12 @@
-install.packages(c("shiny", "shinyhelper", "rsconnect"))
+install.packages(c("shiny", "shinyhelper", "rsconnect", "plotly", "zoo", "httr", "magrittr"), dependencies = TRUE)
 
 library("shiny")
 library("shinyhelper")
 library("rsconnect")
+library("plotly")
+library("zoo")
+library("httr")
+library("magrittr")
 
 SHINYAPPS_NAME = Sys.getenv("SHINYAPPS_NAME")
 SHINYAPPS_TOKEN = Sys.getenv("SHINYAPPS_TOKEN")
