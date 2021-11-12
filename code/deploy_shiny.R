ce4e354ead1a6a461d7f397ac8ae83c0fdc8bc26
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-11-12 15:02:34 +0100

    added script to deploy shiny app

---
+++ code/deploy_shiny.R
@@ -0,0 +1,13 @@
+install.packages(c("shiny", "shinyhelper", "rsconnect"))
+
+library("shiny")
+library("shinyhelper")
+library("rsconnect")
+
+SHINYAPPS_NAME = Sys.getenv("SHINYAPPS_NAME")
+SHINYAPPS_TOKEN = Sys.getenv("SHINYAPPS_TOKEN")
+SHINYAPPS_SECRET = Sys.getenv("SHINYAPPS_SECRET")
+
+
+setAccountInfo(name=SHINYAPPS_NAME, token=SHINYAPPS_TOKEN, secret=SHINYAPPS_SECRET)
+deployApp(appDir = "../nowcast_viz_de", appName = "github-viz-test")
