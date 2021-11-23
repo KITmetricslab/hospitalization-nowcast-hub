f067f6b19bc9ad3eb9062df133dc02b6a10fdfdf
jbracher <jbracher@outlook.de>
2021-11-23 12:21:49 +0100

    Extend ensemble code slightly

--- code/ensemble/generate_ensembles.R
+++ code/ensemble/generate_ensembles.R
@@ -1,12 +1,15 @@
 # generate ensemble forecasts
 
 # needs to be run from repository root folder
-# setwd("/home/johannes/Documents/Projects/hospitalization-nowcast-hub")
+# setwd("/home/johannes/Documents/Projects/hospitalization-nowcast-hub_fork")
 
 forecast_date <- Sys.Date()
 
 teams <- list.dirs("data-processed", full.names = FALSE)
 teams <- teams[nchar(teams) > 0]
+teams <- teams[!grepl("NowcastHub", teams)]
+
+included_models <- character(0)
 
 # read in all forecasts:
 all_forecasts <- NULL
@@ -17,11 +20,11 @@ for(te in teams){
     dat_temp <- read.csv(paste0("data-processed/", te, "/", file_forecast_date),
                          colClasses = c("forecast_date" = "Date", "target_end_date" = "Date"))
     dat_temp$X <- NULL
-    dat_temp <- subset(dat_temp, target_end_date <= forecast_date - 2)
+    # dat_temp <- subset(dat_temp, target_end_date <= forecast_date - 2)
     
     # plausibility checks:
     tab_locations <- table(dat_temp$location)
-    locations_to_remove <- names(tab_locations)[tab_locations < (27*8)]
+    locations_to_remove <- names(tab_locations)[tab_locations < (25*8)]
     if(length(locations_to_remove) > 0){
       dat_temp <- subset(dat_temp, !(location %in% locations_to_remove))
       cat("   Removing locations", locations_to_remove, "for", te, "as nowcasts seem incomplete.\n")
@@ -42,8 +45,8 @@ for(te in teams){
     check4 <- all(c(0.025, 0.25, 0.5, 0.75, 0.9, 0.975) %in% dat_temp$quantile) & "mean" %in% dat_temp$type
     if(!check4) cat("   Missing quantile levels or means in", te, "\n")
     
-    check5 <- all(paste(-2:-28, "day ahead inc hosp") %in% dat_temp$target)
-    if(!check5) cat("   Not all horizons between -2 and -28 days addressed in", te, "\n")
+    check5 <- all(paste(0:-28, "day ahead inc hosp") %in% dat_temp$target)
+    if(!check5) cat("   Not all horizons between 0 and -28 days addressed in", te, "\n")
     
     if(check1 & check2 & check3 & check4 & check5){
       cat("Including", te, "\n")
@@ -52,14 +55,15 @@ for(te in teams){
       }else{
         all_forecasts <- rbind(all_forecasts, dat_temp)
       }
+      included_models <- c(included_models, te)
     }else{
-      cat("   Not including", te, "\n")
+      cat("Not including", te, "\n")
     }
     
   }else{
     cat("   No file found for", te, "at nowcast date", as.character(forecast_date), "\n")
   }
-  
+  cat("------------\n")
 }
 
 # split into means and quantiles:
@@ -92,7 +96,7 @@ mean_ensemble <- mean_ensemble[order(mean_ensemble$target_end_date), ]
 
 dir.create("data-processed/NowcastHub-MeanEnsemble/")
 write.csv(mean_ensemble, file = paste0("data-processed/NowcastHub-MeanEnsemble/",
-                                       forecast_date, "-NowcastHub-MeanEnsemble.csv"))
+                                       forecast_date, "-NowcastHub-MeanEnsemble.csv"), row.names = FALSE)
 
 
 # compute median ensemble:
@@ -121,4 +125,23 @@ median_ensemble <- median_ensemble[order(median_ensemble$target_end_date), ]
 
 dir.create("data-processed/NowcastHub-MedianEnsemble/")
 write.csv(median_ensemble, file = paste0("data-processed/NowcastHub-MedianEnsemble/",
-                                       forecast_date, "-NowcastHub-MedianEnsemble.csv"))
+                                       forecast_date, "-NowcastHub-MedianEnsemble.csv"), row.names = FALSE)
+
+# store included models:
+if(file.exists("code/ensemble/documentation_members.csv")){
+  dat_included_models <- read.csv("code/ensemble/documentation_members.csv", colClasses = c("nowcast_date" = "Date"))
+  if(forecast_date %in% dat_included_models$forecast_date){
+    warning("There is already an entry with the same forecast_date in documentation_members.csv")
+  }else{
+    to_add <- data.frame(forecast_date = forecast_date,
+                         included_models = paste(included_models, collapse = ";"))
+    dat_included_models <- rbind(dat_included_models,
+                                 to_add)
+    write.csv(dat_included_models, file = "code/ensemble/documentation_members.csv", row.names = FALSE)
+  }
+}else{
+  dat_included_models <- data.frame(forecast_date = forecast_date,
+                       included_models = paste(included_models, collapse = ";"))
+  write.csv(dat_included_models, file = "code/ensemble/documentation_members.csv", row.names = FALSE)
+}
+ 
