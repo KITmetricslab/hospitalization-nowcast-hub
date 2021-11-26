72fd805be9eec6daa8810710d5bec174260c1ef4
jbracher <jbracher@outlook.de>
2021-11-26 18:09:27 +0100

    Adding processing codes.

---
+++ code/fetch_nowcasts/epiforecasts_url2nowcast.R
@@ -0,0 +1,40 @@
+setwd("/home/johannes/Documents/Projects/hospitalization-nowcast-hub_fork/code/fetch_nowcasts")
+
+forecast_dates <- seq(from = as.Date("2021-10-01"),
+                      to = as.Date("2021-11-21"),
+                      by = 1)
+
+for(i in seq_along(forecast_dates)){
+  
+  forecast_date <- forecast_dates[i]
+  cat("Processing", as.character(forecast_date), "...\n")
+  
+  url <- paste0("https://raw.githubusercontent.com/epiforecasts/eval-germany-sp-nowcasting/main/data/nowcasts/submission/independent/", 
+                forecast_date, ".csv")
+  
+  dat <- read.csv(url)
+  
+  dat$mean <- NULL
+  
+  subs_median <- subset(dat, type == "median")
+  subs_median$type <- "quantile"
+  subs_median$quantile <- 0.5
+  
+  subs_not_median <- subset(dat, type != "median")
+  
+  dat <- rbind(subs_median, subs_not_median)
+  
+  if(as.Date(forecast_date) < Sys.Date()){
+    dir.create("../../data-processed_retrospective/Epiforecasts-independent/")
+    dir_to_store <- paste0("../../data-processed_retrospective/Epiforecasts-independent/", forecast_date, "-Epiforecasts-independent.csv")
+  }else{
+    dir.create("../../data-processed/Epiforecasts-independent/")
+    dir_to_store <- paste0("../../data-processed/Epiforecasts-independent/", forecast_date, "-Epiforecasts-independent.csv")
+  }
+  
+  write.csv(dat, 
+            file = dir_to_store,
+            row.names = FALSE)
+}
+
+
