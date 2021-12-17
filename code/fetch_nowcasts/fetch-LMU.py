3a3ec63cfb145df3966c3ed7d180811a4a758476
dwolffram <25206417+dwolffram@users.noreply.github.com>
2021-12-17 15:24:37 +0100

    adjusted path

--- code/fetch_nowcasts/fetch-LMU.py
+++ code/fetch_nowcasts/fetch-LMU.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 
 today = pd.to_datetime('today').date()
-filename = f'LMU_StaBLab-GAM_nowcast/{today}-LMU_StaBLab-GAM_nowcast.csv'
+filename = f'data-processed/LMU_StaBLab-GAM_nowcast/{today}-LMU_StaBLab-GAM_nowcast.csv'
 
 if os.path.exists(filename):
     print(f'Nowcast for today ({today}) has already been added.')
 else:
-    df = pd.read_csv('https://raw.githubusercontent.com/MaxWeigert/hospitalization-nowcast-hub/main/data-processed/' + filename)
-    df.to_csv('./data-processed/' + filename, index = False)
+    df = pd.read_csv('https://raw.githubusercontent.com/MaxWeigert/hospitalization-nowcast-hub/main/' + filename)
+    df.to_csv(filename, index = False)
     
