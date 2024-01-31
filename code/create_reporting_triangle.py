ce9cceef0b1256d1d04d4b30e326f22a6edde883
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2024-01-31 10:24:23 +0100

    Update create_reporting_triangle.py

--- code/create_reporting_triangle.py
+++ code/create_reporting_triangle.py
@@ -4,7 +4,7 @@ from tqdm.auto import tqdm
 tqdm.pandas()
 
 path = Path('../data-truth/COVID-19/deconvoluted/')
-files = sorted([f.name for f in path.glob('**/*')])[:-15]
+files = sorted([f.name for f in path.glob('**/*')])
 dates = [f[:10] for f in files]
 
 dfs = []
