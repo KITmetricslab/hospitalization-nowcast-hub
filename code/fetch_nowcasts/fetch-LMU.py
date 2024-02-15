dccd7239a3ae0d3498b6df0bc0cb6d5f31abc90d
Daniel Wolffram <25206417+dwolffram@users.noreply.github.com>
2024-02-15 13:33:39 +0100

    Update fetch-LMU.py

--- code/fetch_nowcasts/fetch-LMU.py
+++ code/fetch_nowcasts/fetch-LMU.py
@@ -7,6 +7,6 @@ filename = f'data-processed/LMU_StaBLab-GAM_nowcast/{today}-LMU_StaBLab-GAM_nowc
 if os.path.exists(filename):
     print(f'Nowcast for today ({today}) has already been added.')
 else:
-    df = pd.read_csv('https://raw.githubusercontent.com/MaxWeigert/hospitalization-nowcast-hub/main/' + filename)
+    df = pd.read_csv('https://raw.githubusercontent.com/milapfander/hospitalization-nowcast-hub/main/' + filename)
     df.to_csv(filename, index = False)
     
